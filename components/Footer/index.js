7d547fd9598b5d3a4073f53a5d278de301863d16
Cristhian Benitez <cristhiantilleria@gmail.com>
2023-02-05 18:45:56 -0500

    update

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,7 +25,7 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{' '}
-        <Link href="http://www.cristhianbenitez.me">
+        <Link href="https://chetanverma.com/">
           <a className="underline underline-offset-1">Cristhian Benitez</a>
         </Link>
       </h1>
