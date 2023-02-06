b5742d4beda3b8ae3870d090d94a2874b40a91fc
Cristhian Benitez <cristhiantilleria@gmail.com>
2023-02-05 19:45:56 -0500

    credits

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,9 +25,7 @@ const Footer = ({}) => {
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{' '}
         <Link href="https://chetanverma.com/">
-          <span className="underline underline-offset-1">
-            Cristhian Benitez
-          </span>
+          <span className="underline underline-offset-1">Chetan Verma</span>
         </Link>
       </h1>
     </>
