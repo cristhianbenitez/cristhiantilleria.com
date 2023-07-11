73000050c6b4d1a0cd95e0e7ffc9134ad50d7472
Cristhian Benitez <cristhiantilleria@gmail.com>
2023-07-10 21:06:14 -0400

    change

--- app/work/[work]/page.js
+++ app/work/[work]/page.js
@@ -11,30 +11,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import Link from 'next/link';
 
-import ImageViewer from 'react-simple-image-viewer';
-
 export default function Work({}) {
-  const [currentImage, setCurrentImage] = React.useState(0);
-  const [isViewerOpen, setIsViewerOpen] = React.useState(false);
-  const images = [
-    'http://localhost:3000/_next/image?url=%2Fassets%2Fmockup.jpg&w=1920&q=75',
-    'http://localhost:3000/_next/image?url=%2Fassets%2Fmockup.jpg&w=1920&q=75',
-    'http://localhost:3000/_next/image?url=%2Fassets%2Fmockup.jpg&w=1920&q=75',
-    'http://localhost:3000/_next/image?url=%2Fassets%2Fmockup.jpg&w=1920&q=75'
-  ];
-
-  // TODO ADD THE MODAL FOR IMAGE VIEWER
-
-  const openImageViewer = React.useCallback((index) => {
-    setCurrentImage(index);
-    setIsViewerOpen(true);
-  }, []);
-
-  const closeImageViewer = () => {
-    setCurrentImage(0);
-    setIsViewerOpen(false);
-  };
-
   return (
     <div>
       <section className={styles.projectInformation}>
@@ -57,32 +34,6 @@ export default function Work({}) {
           </p>
         </div>
       </section>
-
-      <div>
-        {images.map((src, index) => (
-          <img
-            src={src}
-            onClick={() => openImageViewer(index)}
-            width="300"
-            key={index}
-            style={{ margin: '2px' }}
-            alt=""
-          />
-        ))}
-
-        {isViewerOpen && (
-          <ImageViewer
-            src={images}
-            currentIndex={currentImage}
-            onClose={closeImageViewer}
-            disableScroll={false}
-            backgroundStyle={{
-              backgroundColor: 'rgba(0,0,0,0.9)'
-            }}
-            closeOnClickOutside={true}
-          />
-        )}
-      </div>
 
       <Swiper
         slidesPerView={1}
