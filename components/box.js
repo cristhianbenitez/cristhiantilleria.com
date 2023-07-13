6f94c36a1656701bb545f686874d2e6a04d5ccc3
Cristhian Benitez <cristhiantilleria@gmail.com>
2023-07-13 16:59:18 -0400

    slider

--- components/box.js
+++ components/box.js
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { forwardRef } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
--- components/slider.js
+++ components/slider.js
@@ -2,7 +2,6 @@
 import React from 'react';
 
 import styles from './slider.module.css';
-import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -10,23 +9,21 @@ import Image from 'next/image';
 
 export default function Slider(props) {
   return (
-    <>
-      <div className={styles.carousel}>
-        <Carousel
-          showThumbs={false}
-          showStatus={false}
-          useKeyboardArrows={true}
-          infiniteLoop={true}
-          autoPlay={true}
-          interval={3000}
-        >
-          {props.images.map((URL, index) => (
-            <div className={styles.carousel__item}>
-              <Image alt="sample_file" src={URL} key={URL} fill priority />
-            </div>
-          ))}
-        </Carousel>
-      </div>
-    </>
+    <div className={styles.carousel}>
+      <Carousel
+        showThumbs={false}
+        showStatus={false}
+        useKeyboardArrows={true}
+        infiniteLoop={true}
+        autoPlay={true}
+        interval={2000}
+      >
+        {props.images.map((URL) => (
+          <div className={styles.carousel__item} key={URL}>
+            <Image alt="sample_file" src={URL} fill priority />
+          </div>
+        ))}
+      </Carousel>
+    </div>
   );
 }
