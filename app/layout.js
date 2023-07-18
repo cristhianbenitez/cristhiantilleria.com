5ede4f32cb6966f300bc663f30fe9111af723762
Cristhian Benitez <cristhiantilleria@gmail.com>
2023-07-17 23:10:43 -0400

    comments

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Navigation from '@/components/navigation';
 import { Inter } from 'next/font/google';
 
 import './global.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,8 +25,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  console.log(
+    "===================== It ain't much, but is honest work Developed by Cristhian Tilleria ====================="
+  );
   return (
     <html lang="en">
+      {/* It ain't much, but is honest work
+      Developed by Cristhian Tilleria
+      */}
+
       <Analytics />
       <body className={`${inter.className} ${styles.container}`}>
         <Navigation />
