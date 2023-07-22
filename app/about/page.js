a6f5334bfb4f7fb975d666dad37de745e6ac8067
Cristhian Benitez <cristhiantilleria@gmail.com>
2023-07-22 12:19:07 -0400

    yes

--- app/about/page.js
+++ app/about/page.js
@@ -29,14 +29,15 @@ export default function About() {
       </section>
       <section className={styles.about__column} grid-col="2">
         <h2 className={styles.about__headline}>
-          Cristhian, a Freelance Graphic Designer based in New York, who has
-          developed a strong passion for design. With an insatiable thirst for
-          knowledge, he constantly seeks improvement. Previous to his design
-          education, he believed creativity was exclusive to a select few, but
-          his immersion in the design world unveiled a deep fascination for art
-          and design. Motivated by a strong work ethic, Cristhian wholeheartedly
-          dedicates himself to crafting brand identities, websites, and print
-          designs. Excited to contribute meaningfully to projects that help people.
+          I am a New York-based Freelance Graphic Designer with a newfound
+          passion for design and a strong interest in communication. I am always
+          seeking to improve as a person and acquire more knowledge to better
+          understand life, despite all its challenges. My immersion in the
+          design world has unveiled a fascination for art and design. It has
+          also helped me see the struggles that we face as humans and how, as a
+          designer, I can make a small contribution to a more connected world.
+          Motivated by a strong work ethic, I wholeheartedly dedicate myself to
+          my craft.
         </h2>
         {/* <div className={styles.about__services}>
           <h3 className={styles.about__services__title}>Services:</h3>
@@ -52,4 +53,15 @@ export default function About() {
       </section>
     </PageWrapper>
   );
+}
+
+{
+  /* Cristhian, a Freelance Graphic Designer based in New York, who has
+developed a strong passion for design. With an insatiable thirst for
+knowledge, he constantly seeks improvement. Previous to his design
+education, he believed creativity was exclusive to a select few, but
+his immersion in the design world unveiled a deep fascination for art
+and design. Motivated by a strong work ethic, Cristhian wholeheartedly
+dedicates himself to crafting brand identities, websites, and print
+designs. Excited to contribute meaningfully to projects that help people. */
 }
