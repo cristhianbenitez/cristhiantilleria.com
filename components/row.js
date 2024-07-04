50d266566113860bad93f7c42ae2782c89d17c41
Cristhian Benitez <cristhiantilleria@gmail.com>
2024-07-03 23:34:32 -0400

    update in level up projects

--- components/row.js
+++ components/row.js
@@ -5,7 +5,7 @@ import React from 'react';
 import styles from './row.module.css';
 import { motion, useInView } from 'framer-motion';
 
-const Row = ({ leftImage, rightImage, fullImage }) => {
+const Row = ({ leftImage, rightImage, fullImage, video }) => {
   const ref = React.useRef(null);
   const isInView = useInView(ref, {
     once: true,
@@ -18,50 +18,72 @@ const Row = ({ leftImage, rightImage, fullImage }) => {
     transition: 'all 0.9s ease-in'
   };
 
-  return fullImage ? (
-    <div className={styles.fullRow}>
-      <Image
-        ref={ref}
-        src={fullImage}
-        width={9999}
-        height={9999}
-        style={imageStyle}
-        className={styles.row_image}
-      />
-    </div>
-  ) : (
-    <div className={styles.row}>
-      <div>
-        {leftImage && (
-          <div className={styles.row_image_container} grid-col="1">
-            <Image
-              src={leftImage}
-              width={9999}
-              height={9999}
-              ref={ref}
-              style={imageStyle}
-              className={styles.row_image}
-            />
-          </div>
-        )}
+  if (fullImage) {
+    return (
+      <div className={styles.fullRow}>
+        <Image
+          ref={ref}
+          src={fullImage}
+          width={9999}
+          height={9999}
+          style={imageStyle}
+          className={styles.row_image}
+        />
       </div>
-      <div>
-        {rightImage && (
-          <div className={styles.row_image_container} grid-col="2">
-            <Image
-              src={rightImage}
-              width={9999}
-              height={9999}
-              ref={ref}
-              style={imageStyle}
-              grid-col="2"
-              className={styles.row_image}
-            />
-          </div>
-        )}
+    );
+  }
+  if (leftImage || rightImage) {
+    return (
+      <div className={styles.row}>
+        <div>
+          {leftImage && (
+            <div className={styles.row_image_container} grid-col="1">
+              <Image
+                src={leftImage}
+                width={9999}
+                height={9999}
+                ref={ref}
+                style={imageStyle}
+                className={styles.row_image}
+              />
+            </div>
+          )}
+        </div>
+        <div>
+          {rightImage && (
+            <div className={styles.row_image_container} grid-col="2">
+              <Image
+                src={rightImage}
+                width={9999}
+                height={9999}
+                ref={ref}
+                style={imageStyle}
+                grid-col="2"
+                className={styles.row_image}
+              />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  if (video) {
+    return (
+      <div className={styles.fullRow}>
+        <video
+          ref={ref}
+          className={styles.row_image}
+          autoPlay
+          loop
+          muted
+          style={imageStyle}
+        >
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
 };
 
 export default Row;
